a486a09f785e0e4cea4978e21c582c5662add2d0
jerome85 <jerome.nourrain85@gmail.com>
2015-04-29 13:29:11 -0400

    changement de branche

--- js/script.js
+++ js/script.js
@@ -13,15 +13,27 @@ $(document).ready(function() {
 
 	$('#prev').click(function(){
 		compteur = ((compteur==0)?5:compteur-1);
+
+		var moins = ((compteur==0)?5:compteur-1);
+		var plus = ((compteur==5)?0:compteur+1);
+
 		$('.clear').toggleClass('clear grayscale');
 		$('.grayscale').toggleClass('grayscale grayscale');
 		$('#'+compteur).toggleClass('grayscale clear');
+		$('.'+(moins)).attr('href', '#');
+		$('.'+(plus)).attr('href', '#');
 	});
 
 	$('#next').click(function(){
 		compteur = ((compteur==5)?0:compteur+1);
+
+		var moins = ((compteur==0)?5:compteur-1);
+		var plus = ((compteur==5)?0:compteur+1);
+
 		$('.clear').toggleClass('clear grayscale');
 		$('.grayscale').toggleClass('grayscale grayscale');
 		$('#'+compteur).toggleClass('grayscale clear');
+		$('.'+(moins)).attr('href', '#').removeClass('popup-youtube');
+		$('.'+(plus)).attr('href', '#').removeClass('popup-youtube');
 	});
 });
